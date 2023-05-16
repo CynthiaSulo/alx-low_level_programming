fb20a6b1174a4bf8639f51e629e03c3f82d0cd39
CynthiaSulo <cynthiasulo2@gmail.com>
2023-05-16 13:03:51 -0400

    0x0B. C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,30 +10,24 @@
  */
 int count_words(char *str)
 {
-        int count = 0, i = 0;
+	int count = 0, i = 0;
 
-        if (str == NULL)
-                return (0);
-
-        while (isspace(str[i]))
-                i++;
-
-        if (str[i] == '\0')
-                return (0);
-
-        while (str[i])
-        {
-                while (isspace(str[i]))
-                        i++;
-
-                if (str[i])
-                        count++;
-
-                while (str[i] && !isspace(str[i]))
-                        i++;
-        }
-
-        return (count);
+	if (str == NULL)
+		return (0);
+	while (isspace(str[i]))
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		while (isspace(str[i]))
+			i++;
+		if (str[i])
+			count++;
+		while (str[i] && !isspace(str[i]))
+			i++;
+	}
+	return (count);
 }
 
 /**
@@ -43,12 +37,11 @@ int count_words(char *str)
  */
 int word_length(char *str)
 {
-        int len = 0;
+	int len = 0;
 
-        while (str[len] && !isspace(str[len]))
-                len++;
-
-        return (len);
+	while (str[len] && !isspace(str[len]))
+		len++;
+	return (len);
 }
 
 /**
@@ -58,48 +51,42 @@ int word_length(char *str)
  */
 char **strtow(char *str)
 {
-        int i, j, k, n, word_count;
-        char **words;
+	int i, j, k, n, word_count;
+	char **words;
 
-        if (str == NULL || str[0] == '\0')
-                return (NULL);
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	word_count = count_words(str);
+	if (word_count == 0)
+		return (NULL);
+	words = malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (isspace(str[i]))
+			i++;
+		if (str[i] == '\0')
+			break;
+		n = word_length(&str[i]);
+		words[j] = malloc((n + 1) * sizeof(char));
+		if (words[j] == NULL)
+		{
+			while (--j >= 0)
+				free(words[j]);
+			free(words);
+			return (NULL);
+		}
+		for (k = 0; k < n; k++)
+			words[j][k] = str[i + k];
 
-        word_count = count_words(str);
-        if (word_count == 0)
-                return (NULL);
+		words[j][k] = '\0';
 
-        words = malloc((word_count + 1) * sizeof(char *));
-        if (words == NULL)
-                return (NULL);
-
-        i = 0;
-        j = 0;
-        while (str[i])
-        {
-                while (isspace(str[i]))
-                        i++;
-
-                if (str[i] == '\0')
-                        break;
-
-                n = word_length(&str[i]);
-                words[j] = malloc((n + 1) * sizeof(char));
-                if (words[j] == NULL)
-                {
-                        while (--j >= 0)
-                                free(words[j]);
-                        free(words);
-                        return (NULL);
-                }
-
-                for (k = 0; k < n; k++)
-                        words[j][k] = str[i + k];
-                words[j][k] = '\0';
-
-                i += n;
-                j++;
-        }
-
-        words[j] = NULL;
-        return (words);
+		i += n;
+		j++;
+	}
+	words[j] = NULL;
+	return (words);
 }
