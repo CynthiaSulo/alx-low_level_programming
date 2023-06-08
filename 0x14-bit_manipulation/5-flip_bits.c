37fd088ca93724d6f45541c79b10ee6a0eebb1fa
CynthiaSulo <cynthiasulo2@gmail.com>
2023-06-08 08:53:16 -0400

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits needed to flip
+ * @n: The first parameter
+ * @m: The second parameter
+ *
+ * Return: The number of bits needed to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff = n ^ m;
+	unsigned int count = 0;
+
+	while (diff > 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+
+	return (count);
+}
